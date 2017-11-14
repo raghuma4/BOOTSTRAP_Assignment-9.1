92336e096c9cc3ce77a92cdb4ac7711a3795da77
raghuma4 <32066962+raghuma4@users.noreply.github.com>
2017-11-14 22:57:12 +0530

    Add files via upload

---
+++ ButtonPopover.js
@@ -0,0 +1,7 @@
+
+// using jQuery pre-defined popover() tp display popover messages
+$(document).ready(function()
+{
+	// apply popover() on the elements having data-toggle="popover"
+	$('[data-toggle="popover"').popover();
+});
